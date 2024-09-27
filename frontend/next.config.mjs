b60114380e40b5315d78698799b5eed79ce27efb
nailu <contact@nailu.fr>
2024-09-27 22:33:39 +0200

    Add domain for next/image

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -6,6 +6,11 @@ const nextConfig = {
                 protocol: 'http',
                 hostname: '127.0.0.1',
                 port: '1337',
+
+            },
+            {
+                protocol: 'https',
+                hostname: process.env.HOST || '',
             },
         ],
     },
